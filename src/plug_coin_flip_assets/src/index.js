276ac5d2793ad3aad27d27ae14406c54cb2bb26d
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-07-28 22:12:35 -0500

    Added router logic & basic config

--- src/plug_coin_flip_assets/src/index.js
+++ src/plug_coin_flip_assets/src/index.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { render } from "react-dom";
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import ConnectionBadge from './components/ConnectionBadge';
 
-const MyHello = () =>  {
+import Background from '../assets/background.jpeg';
+import TitleImg from '../assets/title-image.png';
+import '../assets/main.css';
+
+const App = () =>  {
+  const [auth, setAuth] = useState(false);
+  const [principalId, setPrincipalId] = useState('');
+
   return (
-    <h1>TestY</h1>
+    <div className='app'>
+      <img className='background' src={Background} />
+      <div className='content'>
+        <img className='title-image' src={TitleImg} />
+        <ConnectionBadge principalId={principalId}/>
+        <Router>
+          { auth ? <Redirect to="/" /> : <Redirect to="/auth" /> }
+          <Route path="/auth">
+            <h1>Connect to Start Playing</h1>
+          </Route>
+        </Router>
+      </div>
+    </div>
   );
 };
 
-render(<MyHello />, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
