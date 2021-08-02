0c67c3bad79f85eb7c30894c0dfb2e76a56e8243
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-08-02 15:30:27 -0500

    Added prod url to agent

--- src/plug_coin_flip_assets/src/views/connect/index.js
+++ src/plug_coin_flip_assets/src/views/connect/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PlugConnect from '@psychedelic/plug-connect';
 
 const Connect = ({ handleConnect }) => {
-
-  const network = 'http://localhost:8080';
+  const network = "https://mainnet.dfinity.network/";
   const whitelist = [process.env.PLUG_COIN_FLIP_CANISTER_ID || 'ryjl3-tyaaa-aaaaa-aaaba-cai'];
 
   return (
